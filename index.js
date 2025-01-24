01e3a9ccdbdbae6300c7159b0634257c14b704b1
datadiego <juandiegomariscal@gmail.com>
2025-01-24 16:33:40 +0100

    update

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
+const geoip = require('geoip-lite');
 const db = require('./initdb');
+const fs = require('fs');
+const path = require('path');
 const app = express();
-const PORT = 80;
+const PORT = 8000;
+const logStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 app.use(express.static('public'));
-
-function geoIP(ip) {
-    const geoip = require('geoip-lite');
-    const geo = geoip.lookup(ip);
-    return geo;
-}
+app.use(express.json());
+app.use(morgan('combined', { stream: logStream }));
 
 app.get('/', (req, res) => {
     res.send('Bienvenido a la API de gatitos, envia una peticion GET a /gatitos para obtener una imagen aleatoria.');
@@ -20,7 +21,7 @@ app.get('/gatitos', (req, res) => {
     const userAgent = req.get('User-Agent');
     const date = new Date().toISOString();
     const location = geoIP(ip);
-
+    
     //inserta datos en la base de datos
     const insert = db.prepare('INSERT INTO usuarios (ip, userAgent, date) VALUES (?, ?, ?)');
     insert.run(ip, userAgent, date);
@@ -32,6 +33,7 @@ app.get('/gatitos', (req, res) => {
         "4.jpeg",
         "5.jpeg"
     ];
+    
     const random = Math.floor(Math.random() * gatitos.length);
     const gatito = gatitos[random];
     res.sendFile(`${__dirname}/${gatito}`);
@@ -47,6 +49,12 @@ app.get('/usuarios', (req, res) => {
     res.json(usuarios);
 });
 
+app.post("/collect", (req, res) => {
+    const data = req.body;
+    console.log(data);
+    res.send(data);
+});
+
 app.listen(PORT, () => {
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on port ${PORT}`);
 })
--- select_all.sh
+++ select_all.sh
@@ -3,3 +3,7 @@
 # SELECT * FROM users to usuarios.db
 
 sqlite3 usuarios.db "SELECT * FROM usuarios;"
+
+# SHOW TABLES
+
+sqlite3 usuarios.db ".tables"
